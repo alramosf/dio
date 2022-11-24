1b8672ff1dcdc40d9ca8102cfd2710a92140b5dd
alramosf <106263609+alramosf@users.noreply.github.com>
2022-11-23 23:57:54 -0300

    a

--- dio/src/pages/home/index.jsx
+++ dio/src/pages/home/index.jsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom';
+
+import { Button } from '../../components/Button';
 const Home = () => {
     return (<>
         <h1>Home</h1>
-        <button>Fazer Login</button>
+        <Button title='Teste 1'></Button>
+        <Button variant='secondary' title='Teste 2'></Button> 
+        <Link to ="/login">Fazer Login</Link>
     </>)
 }
 
